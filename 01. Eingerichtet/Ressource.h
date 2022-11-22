f82845067778015502f372ae8eaa5c3680a7b17f
Jonte Gerlach <jontegerlach@gmail.com>
2022-11-22 14:38:21 +0100

    Ressource dynamischer gestaltet

--- 01. Eingerichtet/Ressource.h
+++ 01. Eingerichtet/Ressource.h
@@ -12,17 +12,17 @@ private:
 	static int typc;				//Attribute der Klasse
 	int typ;
 	string name;
-	float menge;
+	int menge;
 
 public:
 
 	~Ressource();					//Destruktor und Konstruktor
-	Ressource(string, float);
-	float getmenge();				//Get und Set Methoden
+	Ressource(string, int);
+	int getmenge();				//Get und Set Methoden
 	string getname();
 	void setname(string);
-	void setmenge(float);
-	void addmenge(float);			//Zum Ändern der Menge
+	void setmenge(int);
+	void addmenge(int);			//Zum Ändern der Menge
 	void darstellen(RenderWindow*); // Ausgabe der Werte in der oberen linken Ecke
 
 };
