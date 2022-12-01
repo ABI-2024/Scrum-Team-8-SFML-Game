b7b238fc0cbfcc6f849171af59c79cb5412ee920
David Nguyen <david.nguyen@schueler.max-eyth-schule.de>
2022-12-01 15:19:18 +0100

    Einfügen des Hintergrundbildes und erstellung der Klasse Grafik

--- 01. Eingerichtet/main.cpp
+++ 01. Eingerichtet/main.cpp
@@ -7,54 +7,66 @@ using namespace std;
 int main() {
 	Textausgabe txtausgabe;
 	Datum date(1, 1, 1999, 1);
+
 	bool g = true, f = true;
+
 	RenderWindow window(VideoMode(1280, 720), "Hold On!");
 	window.setFramerateLimit(30);
+
 	int counter = 0;
+
 	Ressource essen("Essen", 1); //initialisierung von Essen - NICHT ÄNDERN!
 	Ressource wasser("Wasser", 20); //initialisierung von Wasser - NICHT ÄNDERN!
+
 	txtausgabe.setResult(100, 200, 300);
+
+	Texture background; // hintergrund
+	background.loadFromFile("ressources/grafics/hintergrund_tag.png");
+
+	Sprite hintergrund(background);
+
 	while (window.isOpen()) {
 
-		if (essen.getmenge() == 99) {
-			g = false;
-		}
-		else if (essen.getmenge() == 1) {
-			g = true;
-		}
-		if (wasser.getmenge() == 99) {
-			f = true;
-		}
-		else if (wasser.getmenge() == 1) {
-			f = false;
-		}
-		if (f) {
-			wasser.addmenge(-1);
-		}
-		else {
-			wasser.addmenge(1);
-		}
-		if (g) {
-			essen.addmenge(1);
-		}
-		else {
-			essen.addmenge(-1);
-		}
-		if (counter >= 20) {
-			date.add(1);
-			date.update();
-			counter = 0;
-		}
+		//if (essen.getmenge() == 99) {
+		//	g = false;
+		//}
+		//else if (essen.getmenge() == 1) {
+		//	g = true;
+		//}
+		//if (wasser.getmenge() == 99) {
+		//	f = true;
+		//}
+		//else if (wasser.getmenge() == 1) {
+		//	f = false;
+		//}
+		//if (f) {
+		//	wasser.addmenge(-1);
+		//}
+		//else {
+		//	wasser.addmenge(1);
+		//}
+		//if (g) {
+		//	essen.addmenge(1);
+		//}
+		//else {
+		//	essen.addmenge(-1);
+		//}
+		//if (counter >= 20) {
+		//	date.add(1);
+		//	date.update();
+		//	counter = 0;
+		//}
 		counter++;
 		window.clear();
 
+		window.draw(hintergrund);
 		date.display(&window);
 		essen.darstellen(&window);
 		wasser.darstellen(&window);
-		if (!txtausgabe.display(&window)) {
-			txtausgabe.einlesen(newevent());
-			
-		}
+		//if (!txtausgabe.display(&window)) {
+		//	txtausgabe.einlesen(newevent());
+		//	
+		//}
 
 		window.display();
 
