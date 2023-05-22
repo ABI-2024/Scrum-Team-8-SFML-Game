37ffb2d8b99e24c90f94dc83af023d38d4502001
David Nguyen <david.nguyen@schueler.max-eyth-schule.de>
2023-05-22 23:27:24 +0200

    Zufallsgenerator wurde durch neue Version ersetzt, die keine Events 2 mal ausführt

--- 01. Eingerichtet/main.cpp
+++ 01. Eingerichtet/main.cpp
@@ -1,3 +1,4 @@
+#include <random>
 #include <SFML/Graphics.hpp>
 #include <string>
 #include <iostream>
@@ -15,18 +16,20 @@
 using namespace sf;
 using namespace std;
 
+int rando(int a, int b);
 
 void init();
 
 void hintergrund(RenderWindow*);												//Hintergrund und Animationen
 
+void Zufallsgenerator();
 bool EndCheck(Ressource* essen, Ressource* wasser);								//Überprüfung zum Gameover
 void EndLose(RenderWindow*, Audio*, Textausgabe*, Datum*);										//Trigger vom Endscreen bei Niederlage
 void End(RenderWindow*, Audio*, Textausgabe*);											//Trigger vom Normalem Endscreen
 
 int main() {
 	init();
-
+	
 	Datum date(11, 6, 1934, 1);
 
 	RenderWindow window(VideoMode(1280, 720), "Hold On!");
@@ -38,13 +41,15 @@ int main() {
 	sf::Event ev;
 		
 
-
 	Textausgabe txt;
 	Audio music;
 	int counter = 0;
 	Ereignis::setTxt(&txt);
 	Ereignis::setRessources(&essen, &wasser);
 
+	Zufallsgenerator();
+
+	
 	Ereignis::newevent();
 
 
@@ -83,4 +88,47 @@ void init() {
 
 	CSVcontrol::loadConfig();
 	SetEvents::loadFromFile();
+}
+
+void Zufallsgenerator() {
+	int zahl = 0;
+	int counter = 0;
+	bool check[15];
+
+	for (int i = 0; i < 3; i++) {
+
+		fill_n(check, 15, true);
+		counter = 0;
+
+		while (counter < 15) {
+
+			zahl = rando(1, 15);
+
+			if (check[zahl - 1]) {
+
+				check[zahl - 1] = false;
+				Warteschlange::addQueue(zahl + i * 16);
+				counter++;
+			}
+
+		}
+	}
+	
+
+
+	
+}
+
+int rando(int a, int b) {
+
+	std::random_device rd; // obtain a random number from hardware
+	std::mt19937 gen(rd()); // seed the generator
+	if (a > b) {
+		std::uniform_int_distribution<> distr(b, a); // define the range
+		return distr(gen);
+	}
+	else {
+		std::uniform_int_distribution<> distr(a, b); // define the range	
+		return distr(gen);
+	}
 }
